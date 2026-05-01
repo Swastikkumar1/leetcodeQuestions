ef86bd5fda6f965ce5dff05945360b5ceeedaf85
Swastik kumar <143249406+Swastikkumar1@users.noreply.github.com>
2026-05-01 21:36:56 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (51.45%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSoFar = nums[0];
+        int currentMax = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            // Decision: Start a new subarray at current element, 
+            // or add current element to the existing subarray?
+            currentMax = max(nums[i], currentMax + nums[i]);
+            
+            // Update the global maximum if the current subarray is better
+            maxSoFar = max(maxSoFar, currentMax);
+        }
+
+        return maxSoFar;
+    }
+};
