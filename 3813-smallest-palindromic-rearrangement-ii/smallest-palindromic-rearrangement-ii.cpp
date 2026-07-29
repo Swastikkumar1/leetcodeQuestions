39babe88448a855d850893f77404b7a748dd15a9
Swastik kumar <143249406+Swastikkumar1@users.noreply.github.com>
2026-07-30 01:31:53 +0530

    Time: 27 ms (54.20%) | Memory: 17.2 MB (47.33%) - LeetSync

--- 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.cpp
+++ 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.cpp
@@ -1,60 +1,74 @@
 class Solution {
+private:
+    long long comb(long long n, long long m, long long k) {
+        long long res = 1;
+        m = std::min(m, n - m);
+
+        for (long long i = 1; i <= m; i++) {
+            res = res * (n - i + 1) / i;
+            if (res > k) {
+                return k + 1;
+            }
+        }
+        return res;
+    }
+
 public:
-    long long comb(int n, int r, int lim) {
-        r = min(r, n - r);
-        long long ans = 1;
-        for (int i = 1; i <= r; i++) {
-            ans = ans * (n - r + i) / i;
-            if (ans > lim) return lim + 1;
-        }
-        return ans;
-    }
+    std::string smallestPalindrome(std::string s, long long k) {
+        int partition = s.length() / 2;
+        std::vector<int> bucket(26, 0);
 
-    long long calc(vector<int> &cnt, int rem, int lim) {
-        long long ans = 1;
-        for (int x : cnt) {
-            if (!x) continue;
-            ans *= comb(rem, x, lim);
-            if (ans > lim) return lim + 1;
-            rem -= x;
-        }
-        return ans;
-    }
-
-    string smallestPalindrome(string s, int k) {
-        vector<int> cnt(26);
-        for (char c : s) cnt[c - 'a']++;
-
-        int n = s.size(), m = n / 2;
-        string ans(n, ' ');
-
-        for (int i = 0; i < 26; i++) {
-            if (cnt[i] & 1) ans[m] = 'a' + i;
-            cnt[i] /= 2;
+        for (int i = 0; i < partition; i++) {
+            bucket[s[i] - 'a'] += 1;
         }
 
-        if (calc(cnt, m, k) < k) return "";
+        auto permutations = [&](int rem) {
+            long long ways = 1;
+            for (int i = 0; i < 26; i++) {
+                if (bucket[i] == 0) {
+                    continue;
+                }
 
-        for (int i = 0; i < m; i++) {
-            for (int c = 0; c < 26; c++) {
-                if (!cnt[c]) continue;
+                ways *= comb(rem, bucket[i], k);
+                if (ways > k) {
+                    break;
+                }
+                rem -= bucket[i];
+            }
+            return ways;
+        };
 
-                cnt[c]--;
-                long long cur = calc(cnt, m - i - 1, k);
+        std::string left = "";
+        long long startIndex = 1;
 
-                if (cur >= k) {
-                    ans[i] = 'a' + c;
+        for (int pos = 0; pos < partition; pos++) {
+            for (int i = 0; i < 26; i++) {
+                if (bucket[i] == 0) {
+                    continue;
+                }
+
+                bucket[i] -= 1;
+
+                long long ways = permutations(partition - pos - 1);
+                if (startIndex + ways > k) {
+                    left += (char)(i + 'a');
                     break;
                 }
 
-                k -= cur;
-                cnt[c]++;
+                bucket[i] += 1;
+                startIndex += ways;
             }
         }
 
-        for (int i = 0; i < m; i++)
-            ans[n - 1 - i] = ans[i];
+        if (left.length() < partition) {
+            return "";
+        }
 
-        return ans;
+        std::string mid =
+            s.length() % 2 != 0 ? std::string(1, s[partition]) : "";
+        std::string right = left;
+        std::reverse(right.begin(), right.end());
+
+        return left + mid + right;
     }
 };
